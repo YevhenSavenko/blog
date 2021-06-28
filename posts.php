3635588e690e4a6941e8e9d14b1affb3a3a106fa
Yevhen Savenko <eugene.savenko@regulus.team>
2021-06-28 21:47:00 +0300

    Added posts page

---
+++ posts.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <section class="posts container">
+        <div class="my-5">
+            <h2 class="text-center text-uppercase fw-bolder">
+                All Posts
+            </h2>
+        </div>
+        <div class="row justify-content-center mt-5">
+            <div class="posts__item row col-md-8 border p-4 mt-5">
+                <div class="posts__left-part col-md-8 border-end pe-3">
+                    <div class="posts__name text-center text-uppercase fw-bolder fs-4 mb-5">
+                        Name 1
+                    </div>
+                    <div class="posts__body text-break lh-base">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam odit, nemo autem nam veniam asperiores vel quas voluptas eveniet inventore! Aut non dicta unde dolores sunt placeat distinctio quo.
+                    </div>
+                </div>
+                <div class="posts__right-part col-md-4 ps-3 d-flex flex-column justify-content-between">
+                    <div class="posts__date row">
+                        <span class="text-secondary">Date of publication: </span><br>
+                        <span class="text-black-50 mt-2">25-06-2022</span>
+                    </div>
+                    <div class="posts__author row">
+                        <span class="text-secondary">Author: </span><br>
+                        <span class="text-black-50 mt-2"><b>Customer Example</b></span>
+                    </div>
+                </div>
+            </div>
+            <div class="posts__item row col-md-8 border p-4 mt-5">
+                <div class="posts__left-part col-md-8 border-end pe-3">
+                    <div class="posts__name text-center text-uppercase fw-bolder fs-4 mb-5">
+                        Name 1
+                    </div>
+                    <div class="posts__body text-break lh-base">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam odit, nemo autem nam veniam asperiores vel quas voluptas eveniet inventore! Aut non dicta unde dolores sunt placeat distinctio quo.
+                    </div>
+                </div>
+                <div class="posts__right-part col-md-4 ps-3 d-flex flex-column justify-content-between">
+                    <div class="posts__date row">
+                        <span class="text-secondary">Date of publication: </span><br>
+                        <span class="text-black-50 mt-2">25-06-2022</span>
+                    </div>
+                    <div class="posts__author row">
+                        <span class="text-secondary">Author: </span><br>
+                        <span class="text-black-50 mt-2"><b>Customer Example</b></span>
+                    </div>
+                </div>
+            </div>
+            <div class="posts__item row col-md-8 border p-4 mt-5">
+                <div class="posts__left-part col-md-8 border-end pe-3">
+                    <div class="posts__name text-center text-uppercase fw-bolder fs-4 mb-5">
+                        Name 1
+                    </div>
+                    <div class="posts__body text-break lh-base">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eius architecto at saepe expedita id. Aut dicta minus ex cupiditate quidem consequatur non animi, dolores obcaecati ea, illo distinctio inventore at expedita, quis in magnam deserunt cumque nemo explicabo possimus doloribus qui totam. Placeat est ipsa ipsam libero ducimus, voluptatem facilis sapiente, minima praesentium fugiat perferendis soluta amet illo iste dignissimos. Vero velit esse sequi! Non assumenda fugiat doloribus, facere magnam tempore saepe nobis quaerat consequuntur modi minus distinctio autem obcaecati inventore, cumque maiores deleniti omnis ea asperiores dolores reiciendis! Tempora sunt obcaecati, asperiores, aut a minus suscipit veritatis reprehenderit aspernatur eos, dolore saepe repellat maxime. Saepe qui nihil, doloribus perferendis ad iusto quo enim similique sed velit minus optio veritatis nemo! Veritatis iste libero maiores eveniet mollitia aliquam odit soluta magni voluptas accusantium hic consectetur natus ducimus facilis ullam, quos sed! Numquam, ipsum tempora. Recusandae culpa in eveniet aspernatur cupiditate illo nihil, obcaecati molestias voluptates et iste a! Impedit, omnis! Veniam consequuntur dolorem, id dicta ut dolores laborum optio ipsam architecto, hic nostrum magni delectus eaque culpa, adipisci aliquam ad corporis consectetur quia sequi harum. Unde ut rerum quisquam esse veritatis vero obcaecati tempora adipisci dolor repellat! Dignissimos, cumque?
+                    </div>
+                </div>
+                <div class="posts__right-part col-md-4 ps-3 d-flex flex-column justify-content-between">
+                    <div class="posts__date row">
+                        <span class="text-secondary">Date of publication: </span><br>
+                        <span class="text-black-50 mt-2">25-06-2022</span>
+                    </div>
+                    <div class="posts__author row">
+                        <span class="text-secondary">Author: </span><br>
+                        <span class="text-black-50 mt-2"><b>Customer Example</b></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
